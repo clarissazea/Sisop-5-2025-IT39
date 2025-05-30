8079a7878add4e8f00db776998236dd5c2f5e8c4
rafi-adly <rafiadly259@gmail.com>
2025-05-30 20:50:21 +0700

    Update std_lib.c

--- src/std_lib.c
+++ src/std_lib.c
@@ -2,38 +2,127 @@
 
 int div(int a, int b)
 {
-  //TODO: Implementasi pembagian
-  //NOTE: HARUS BISA ANGKA NEGATIF
+    if (b == 0) return 0; 
+    int negatif = 0;
+    if (a < 0) { a = -a; negatif ^= 1; }
+    if (b < 0) { b = -b; negatif ^= 1; }
+
+    int hasil = 0;
+    int cek_pembagi = b;
+    int shift = 0;
+    while ((cek_pembagi << 1) <= a) {
+        cek_pembagi <<= 1;
+        shift++;
+    }
+
+    while (shift >= 0) {
+        if (a >= cek_pembagi) {
+            a -= cek_pembagi;
+            hasil |= (1 << shift);
+        }
+        cek_pembagi >>= 1;
+        shift--;
+    }
+
+    return negatif ? -hasil : hasil;
 }
 
 int mod(int a, int b)
 {
-  //TODO: Implementasi modulus
+  if (b == 0) return -1;
+    int cek_pembagi = b;
+    int shift = 0;
+
+    while ((temp_b << 1) <= a) {
+        cek_pembagi <<= 1;
+        shift++;
+    }
+
+    while (shift >= 0) {
+        if (a >= cek_pembagi) {
+            a -= cek_pembagi;
+        }
+        cek_pembagi >>= 1;
+        shift--;
+    }
+
+    return a;
 }
 
 bool strcmp(char *str1, char *str2)
 {
-  //TODO: Implementasi perbandingan string
+    while (*str1 && (*str1 == *str2)) {
+        str1++;
+        str2++;
+    }
+    return *str1 == *str2;
 }
 
 void strcpy(char *dst, char *src)
 {
-  //TODO: Implementasi penyalinan string
+  while ((*dest++ = *src++));
 }
 
 void clear(byte *buf, unsigned int size)
 {
-  //TODO: Implementasi pembersihan buffer
+    unsigned char *p = (unsigned char *)mem;
+    for (int i = 0; i < len; i++) {
+        p[i] = 0;
+    }
 }
 
 void atoi(char *str, int *num)
 {
-  //TODO: Implementasi konversi string ke integer
-  //NOTE: HARUS BISA ANGKA NEGATIF
+    int result = 0;
+    int sign = 1;
+
+    while (*str == ' ') str++;
+
+    if (*str == '-') {
+        sign = -1;
+        str++;
+    } else if (*str == '+') {
+        str++;
+    }
+
+    while (*str >= '0' && *str <= '9') {
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+
+    *num = result * sign;
 }
 
 void itoa(int num, char *str)
 {
-  //TODO: Implementasi konversi integer ke string
-  //NOTE: HARUS BISA ANGKA NEGATIF
+    int i = 0;
+    int is_negative = 0;
+
+    if (num == 0) {
+        str[i++] = '0';
+        str[i] = '\0';
+        return;
+    }
+
+    if (num < 0) {
+        is_negative = 1;
+        num = -num;
+    }
+
+    while (num > 0) {
+        str[i++] = (num % 10) + '0';
+        num /= 10;
+    }
+
+    if (is_negative) {
+        str[i++] = '-';
+    }
+
+    str[i] = '\0';
+
+    for (int j = 0, k = i - 1; j < k; j++, k--) {
+        char temp = str[j];
+        str[j] = str[k];
+        str[k] = temp;
+    }
 }
