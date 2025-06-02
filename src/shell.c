e878f485b516f8456a06fe2d54606c8137457248
Ahmad Fawwaz <92526178+fawwazjs@users.noreply.github.com>
2025-06-02 16:28:44 +0700

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -39,4 +39,42 @@ void parseCommand(char *buf, char *cmd, char arg[2][64]) {
     }
 }
 
-// Tambahkan fungsi bila perlu
+// Menambahkan fungsi
+void executeCommand(char *cmd, char arg[2][64]) {
+  if (strCompare(cmd, "echo") == 0) {
+    printString(arg[0]);
+    printString("\n");
+  } else if (strCompare(cmd, "clear") == 0) {
+    clearScreen();
+    setTextColor(0x07); // White (default)
+    strCopy(currentPrompt, "user>");
+  } else if (strCompare(cmd, "help") == 0) {
+    printString("Available commands:\n");
+    printString("  echo [text]              - Prints text\n");
+    printString("  clear                    - Clears screen and reset color\n");
+    printString("  grandcompany [name]      - Join a Grand Company\n");
+    printString("     maelstrom, twinadder, immortalflames\n");
+  } else if (strCompare(cmd, "grandcompany") == 0) {
+    if (strCompare(arg[0], "maelstrom") == 0) {
+      clearScreen();
+      setTextColor(0x04); // Red
+      strCopy(currentPrompt, "user@Storm>");
+    } else if (strCompare(arg[0], "twinadder") == 0) {
+      clearScreen();
+      setTextColor(0x0E); // Yellow
+      strCopy(currentPrompt, "user@Serpent>");
+    } else if (strCompare(arg[0], "immortalflames") == 0) {
+      clearScreen();
+      setTextColor(0x01); // Blue
+      strCopy(currentPrompt, "user@Flame>");
+    } else {
+      printString("Unknown Grand Company: ");
+      printString(arg[0]);
+      printString("\n");
+    }
+  } else {
+    printString("Command not found: ");
+    printString(cmd);
+    printString("\n");
+  }
+}
