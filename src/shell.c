ab69ab1aee926a3334ca4379408622fcaf1d51a5
rafi-adly <rafiadly259@gmail.com>
2025-06-02 08:30:12 +0700

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -6,15 +6,37 @@ void shell(){
   char buf[128];
   printString("Welcome to EorzeOS!\n");
   while (true) {
-    printString("> ");
+    printString("user> ");
     readString(buf);
     printString(buf);
-    printString("\n");
+    printString("\r\n");
   }
 }
 
 void parseCommand(char *buf, char *cmd, char arg[2][64]) {
-  //TODO: Implementasi fungsi untuk mem-parsing perintah
+  int argCount = 0;
+    char *p = buf;
+    int i;
+    while (*p && (*p == ' ' || *p == '\t')) p++;
+    i = 0;
+    while (*p && *p != ' ' && *p != '\t' && i < 63) {
+        cmd[i++] = *p++;
+    }
+    cmd[i] = '\0';
+    while (argCount < 2) {
+        while (*p && (*p == ' ' || *p == '\t')) p++;
+        if (!*p) break;
+        i = 0;
+        while (*p && *p != ' ' && *p != '\t' && i < 63) {
+            arg[argCount][i++] = *p++;
+        }
+        arg[argCount][i] = '\0';
+        argCount++;
+    }
+    while (argCount < 2) {
+        arg[argCount][0] = '\0';
+        argCount++;
+    }
 }
 
 // Tambahkan fungsi bila perlu
