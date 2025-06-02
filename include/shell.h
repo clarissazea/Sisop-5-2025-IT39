d5e95ea0f18f1a292fb6bef768fdc83dc6e989d2
Ahmad Fawwaz <92526178+fawwazjs@users.noreply.github.com>
2025-06-02 16:27:51 +0700

    Update shell.h

--- include/shell.h
+++ include/shell.h
@@ -9,4 +9,6 @@ void parseCommand(char *buf, char *cmd, char arg[2][64]);
 // Penambahan fungsi
 void executeCommand(char *cmd, char arg[2][64]);
 
+extern char currentPrompt[32];
+
 #endif // __SHELL_H__
