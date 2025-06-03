a0c23a7495ee039e332acc864da1d99e14b0d767
fawwazjs <wildan406@gmail.com>
2025-06-03 19:36:16 +0700

    Update no 4

--- src/kernel.c
+++ src/kernel.c
@@ -1,51 +1,49 @@
 #include "shell.h"
 #include "kernel.h"
 
+int textColor = 0x07; // default: putih
+
 int main() {
     clearScreen();
     shell();
     while (1);
 }
 
-void printString(char *str)
-{
-  while (*str != '\0') {
-    interrupt(0x10, 0x0E * 256 + *str, 0, 0, 0);
-    str++;
-  }
-}
-
-void readString(char *buf)
-{
-  int i = 0;
-  char c;
-
-  while (1) {
-    c = interrupt(0x16, 0, 0, 0, 0);
-
-    if (c == 0x0D) {
-      buf[i] = '\0';
-      printString("\r\n");
-      break;
-    } else if (c == 0x08) {
-      if (i > 0) {
-        i--;
-        printString("\b \b");
-      }
-    } else {
-      buf[i] = c;
-      i++;
-      interrupt(0x10, 0x0E * 256 + c, 0, 0, 0);
+void printString(char *str) {
+    while (*str != '\0') {
+        interrupt(0x10, (0x0E << 8) + *str, textColor, 0, 0);
+        str++;
     }
-  }
 }
 
-void clearScreen()
-{
-  int i;
-  for (i = 0; i < 25 * 80; i++) {
-    interrupt(0x10, 0x0E * 256 + ' ', 0, 0, 0);
-  }
+void readString(char *buf) {
+    int i = 0;
+    char c;
 
-  interrupt(0x10, 0x0200, 0, 0, 0);
+    while (1) {
+        c = interrupt(0x16, 0, 0, 0, 0);
+
+        if (c == 0x0D) {  // Enter
+            buf[i] = '\0';
+            printString("\r\n");
+            break;
+        } else if (c == 0x08) {  // Backspace
+            if (i > 0) {
+                i--;
+                printString("\b \b");
+            }
+        } else {
+            buf[i] = c;
+            i++;
+            interrupt(0x10, (0x0E << 8) + c, textColor, 0, 0);
+        }
+    }
+}
+
+void clearScreen() {
+    int i;
+    for (i = 0; i < 25 * 80; i++) {
+        interrupt(0x10, (0x0E << 8) + ' ', 0x07, 0, 0);
+    }
+    interrupt(0x10, 0x0200, 0, 0, 0); // Kursor ke kiri atas
 }
--- src/shell.c
+++ src/shell.c
@@ -2,100 +2,120 @@
 #include "kernel.h"
 #include "std_lib.h"
 
-unsigned int seed = 12345;  // seed awal untuk random
-//emang ngaruh yh? ~Adly
+unsigned int seed = 12345;
+
+int textColor = 0x07;
+enum GCType { NONE, STORM, SERPENT, FLAME };
+enum GCType currentGC = NONE;
 
 void shell() {
-  char buf[128];
-  char cmd[64];
-  char args[2][64];
-  char username[64];
-  strcpy(username, "user");
+    char buf[128];
+    char cmd[64];
+    char args[2][64];
+    char username[64];
+    strcpy(username, "user");
 
-  printString("Welcome to EorzeOS!\n");
-  while (1) {
-    printString(username);
-    printString("> ");
-    readString(buf);
-    parseCommand(buf, cmd, args);
-
-    if (strcmp(cmd, "yo")) {
-      printString("gurt\r\n");
-    } else if (strcmp(cmd, "gurt")) {
-      printString("yo\r\n");
-    } else if (strcmp(cmd, "yogurt")) {
-      handleYogurt();
-    } else if (strcmp(cmd, "user")) {
-      if (args[0][0] != '\0') {
-        strcpy(username, args[0]);
-        printString("Username changed to ");
+    printString("Welcome to EorzeOS!\n");
+    while (1) {
         printString(username);
-        printString("\r\n");
-      } else {
-        strcpy(username, "user");
-        printString("Username changed to user\r\n");
-      }
-    } else if (strcmp(cmd, "add") || strcmp(cmd, "sub") || strcmp(cmd, "mul") || strcmp(cmd, "div")) {
-      if (args[0][0] == '\0' || args[1][0] == '\0') {
-        printString("Error: Missing arguments\r\n");
-      } else {
-        int a = atoi(args[0]);
-        int b = atoi(args[1]);
-        int result = 0;
-        char resStr[64];
+        if (currentGC == STORM) printString("@Storm");
+        else if (currentGC == SERPENT) printString("@Serpent");
+        else if (currentGC == FLAME) printString("@Flame");
+        printString("> ");
 
-        if (strcmp(cmd, "add")) {
-          result = a + b;
-        } else if (strcmp(cmd, "sub")) {
-          result = a - b;
-        } else if (strcmp(cmd, "mul")) {
-          result = a * b;
-        } else if (strcmp(cmd, "div")) {
-          result = div(a, b);
+        readString(buf);
+        parseCommand(buf, cmd, args);
+
+        if (strcmp(cmd, "yo")) {
+            printString("gurt\r\n");
+        } else if (strcmp(cmd, "gurt")) {
+            printString("yo\r\n");
+        } else if (strcmp(cmd, "yogurt")) {
+            handleYogurt();
+        } else if (strcmp(cmd, "user")) {
+            if (args[0][0] != '\0') {
+                strcpy(username, args[0]);
+                printString("Username changed to ");
+                printString(username);
+                printString("\r\n");
+            } else {
+                strcpy(username, "user");
+                printString("Username changed to user\r\n");
+            }
+        } else if (strcmp(cmd, "grandcompany")) {
+            if (strcmp(args[0], "maelstrom")) {
+                clearScreen();
+                textColor = 0x0C; // merah terang
+                currentGC = STORM;
+            } else if (strcmp(args[0], "twinadder")) {
+                clearScreen();
+                textColor = 0x0E; // kuning
+                currentGC = SERPENT;
+            } else if (strcmp(args[0], "immortalflames")) {
+                clearScreen();
+                textColor = 0x09; // biru
+                currentGC = FLAME;
+            } else {
+                printString("Error: Unknown Grand Company\r\n");
+            }
+        } else if (strcmp(cmd, "clear")) {
+            clearScreen();
+            textColor = 0x07;
+            currentGC = NONE;
+            printString("Para Grand Company sedih, kamu netral.\r\n");
+        } else if (strcmp(cmd, "add") || strcmp(cmd, "sub") || strcmp(cmd, "mul") || strcmp(cmd, "div")) {
+            if (args[0][0] == '\0' || args[1][0] == '\0') {
+                printString("Error: Missing arguments\r\n");
+            } else {
+                int a = atoi(args[0]);
+                int b = atoi(args[1]);
+                int result = 0;
+                char resStr[64];
+
+                if (strcmp(cmd, "add")) result = a + b;
+                else if (strcmp(cmd, "sub")) result = a - b;
+                else if (strcmp(cmd, "mul")) result = a * b;
+                else if (strcmp(cmd, "div")) result = div(a, b);
+
+                itoa(result, resStr);
+                printString(resStr);
+                printString("\r\n");
+            }
+        } else {
+            printString(buf);
+            printString("\r\n");
         }
-
-        itoa(result, resStr);
-        printString(resStr);
-        printString("\r\n");
-      }
     }
-    else {
-      printString(buf);
-      printString("\r\n");
-    }
-  }
 }
 
 void handleYogurt() {
     char responses[3][100];
-    int count, index;
-    int tick;
+    int count, tick;
 
     strcpy(responses[0], "yo");
     strcpy(responses[1], "ts unami gng </3");
     strcpy(responses[2], "sygau");
     count = 3;
 
-    tick = mod(getBiosTick(),count);
-    // Simulasi mod_safe tanpa %
-    // index = tick - (tick / count) * count;
+    tick = mod(getBiosTick(), count);
 
     printString("gurt> ");
-    // printString(atoi(getBiosTick()));
     printString(responses[tick]);
     printString("\r\n");
 }
 
 void parseCommand(char *buf, char *cmd, char arg[2][64]) {
-  int argCount = 0;
+    int argCount = 0;
     char *p = buf;
     int i;
+
     while (*p && (*p == ' ' || *p == '\t')) p++;
     i = 0;
     while (*p && *p != ' ' && *p != '\t' && i < 63) {
         cmd[i++] = *p++;
     }
     cmd[i] = '\0';
+
     while (argCount < 2) {
         while (*p && (*p == ' ' || *p == '\t')) p++;
         if (!*p) break;
@@ -106,10 +126,9 @@ void parseCommand(char *buf, char *cmd, char arg[2][64]) {
         arg[argCount][i] = '\0';
         argCount++;
     }
+
     while (argCount < 2) {
         arg[argCount][0] = '\0';
         argCount++;
     }
 }
-
-// Tambahkan fungsi bila perlu
