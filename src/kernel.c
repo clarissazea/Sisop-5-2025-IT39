678a3233f358ec48763d54a8bbc6e171aeb816f7
rafi-adly <rafiadly259@gmail.com>
2025-06-03 21:21:30 +0700

    Fixing no 4

--- src/kernel.c
+++ src/kernel.c
@@ -1,49 +1,49 @@
 #include "shell.h"
 #include "kernel.h"
 
-int textColor = 0x07; // default: putih
+int textColor = 0x07; //default (putih)
 
 int main() {
-    clearScreen();
-    shell();
-    while (1);
+  clearScreen(textColor);
+  shell();
 }
 
 void printString(char *str) {
-    while (*str != '\0') {
-        interrupt(0x10, (0x0E << 8) + *str, textColor, 0, 0);
-        str++;
-    }
+  while (*str != '\0') {
+    interrupt(0x10, 0x0E00 + *str, 0, 0, 0);
+    str++;
+  }
 }
 
 void readString(char *buf) {
-    int i = 0;
-    char c;
-
-    while (1) {
-        c = interrupt(0x16, 0, 0, 0, 0);
-
-        if (c == 0x0D) {  // Enter
-            buf[i] = '\0';
-            printString("\r\n");
-            break;
-        } else if (c == 0x08) {  // Backspace
-            if (i > 0) {
-                i--;
-                printString("\b \b");
-            }
-        } else {
-            buf[i] = c;
-            i++;
-            interrupt(0x10, (0x0E << 8) + c, textColor, 0, 0);
-        }
+  int i = 0;
+  char c;
+  while (1) {
+    c = interrupt(0x16, 0x0000, 0, 0, 0) & 0xFF;
+    if (c == 13) {
+      buf[i] = '\0';
+      printString("\r\n");
+      break;
+    } else if (c == 8) {
+      if (i > 0) {
+        i--;
+        printString("\b \b");
+      }
+    } else {
+      buf[i++] = c;
+      interrupt(0x10, 0x0E00 + c, 0, 0, 0);
     }
+  }
 }
 
-void clearScreen() {
-    int i;
-    for (i = 0; i < 25 * 80; i++) {
-        interrupt(0x10, (0x0E << 8) + ' ', 0x07, 0, 0);
-    }
-    interrupt(0x10, 0x0200, 0, 0, 0); // Kursor ke kiri atas
+void clearScreen(textColor) {
+  int i;
+  int screenSize = 80 * 25;
+  for (i = 0; i < screenSize; i++) {
+
+    putInMemory(0xB800, i * 2, ' ');
+    putInMemory(0xB800, i * 2 + 1, textColor);
+  }
+
+  interrupt(0x10, 0x0200, 0, 0, 0);
 }
